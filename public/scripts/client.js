ad580089c15789cba122a71170745e326ac86c89
Jame Jacobs-Smith <jakejacobs-smith1@Jakes-MacBook-Air.local>
2017-01-29 18:40:49 -0600

    trying to add update functionality

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,19 +1,15 @@
-// doc ready function
 $(function(){
   console.log('document loaded');
-  // //get all the tasks from the DB
+
   getTasks();
-  //
-  // // listen for a submit event on the form
+
   $('#task-form').on('submit', addTask);
 
-  //if you click on somethingin book-list and it has the class of "save" run updateBook
-  // $('#task-list').on('click', '.save', updateTask);
   $('#task-list').on('click', '#delete', deleteTask);
   $('#completedTasks').on('click', '#delete', deleteTask);
   $('#task-list').on('click', '#completed', completeTask);
 });
-//
+
 function getTasks() {
   $.ajax({
     url: '/tasks',
@@ -21,7 +17,7 @@ function getTasks() {
     success: displayTasks
   });
 }
-//
+
 function displayTasks(tasks) {
   console.log('Got tasks from the server', tasks);
   $('#task-list').empty();
@@ -31,33 +27,30 @@ function displayTasks(tasks) {
 
       var $li = $('<li></li>');
       $li.data('task', task.task)
-      // var $div = $('<div class = "tasks"></div>');
-      //input type = "text" name = "title" va;ue = "infinite jest"
+
       $li.append('<p class = task>' + task.task + '<p/>');
 
-      //create a button and save the book ID as data on that button
       // var $saveButton = $('<button class = "save">Update</button>');
-      // // //assocaiting the button with the book ID so we know which one is being saved
       // $saveButton.data('id', task.id);
       // $form.append($saveButton);
 
-      var $deleteButton = $('<button class = "tasks" id = "delete">Delete</button>');
+      var $deleteButton = $('<button class = "tasks btn-danger" id = "delete">Delete</button>');
       $deleteButton.data('id', task.id);
       $li.append($deleteButton);
 
-      var $completedButton = $('<button class = "tasks" id = "completed">Completed!</button>');
+      var $completedButton = $('<button class = "tasks btn-success" id = "completed">Completed!</button>');
       $completedButton.data('id', task.id);
       $li.append($completedButton);
 
-      // $li.append($div);
+
       $('#task-list').append($li);
     } else {
       var $li = $('<li></li>');
 
-      //input type = "text" name = "title" value = "infinite jest"
+
       $li.append('<p class = "done">' + task.task + '<p/>');
 
-      var $deleteButton = $('<button class = "tasks" id = "delete">Delete</button>');
+      var $deleteButton = $('<button class = "tasks btn-danger" id = "delete">Delete</button>');
       $deleteButton.data('id', task.id);
       $li.append($deleteButton);
 
@@ -67,13 +60,11 @@ function displayTasks(tasks) {
 }
 
 function addTask(event) {
-  // prevent browser from refreshing
+
   event.preventDefault();
 
-  // get the info out of the form
   var formData = $(this).serialize();
-  $(this).closest('input').val(''); 
-  // send data to server
+  $(this).closest('input').val('');
   $.ajax({
     url: '/tasks',
     type: 'POST',
@@ -126,7 +117,6 @@ function deleteTask(event) {
   var confirmation = confirm("Are you sure you want to delete this task?");
   if (confirmation == true){
       $.ajax({
-        //the url is made from the data on the book itself
         url: '/tasks/' + $(this).data('id'),
         type: 'DELETE',
         success: getTasks
--- routes/tasks.js
+++ routes/tasks.js
@@ -5,26 +5,15 @@ var pg = require("pg");
 
 var config = { database: "upsilon" };
 
-// initialize connection Pool
-// think of as 'how I connect to DB'
 var pool = new pg.Pool(config);
 
 router.get("/", function(req, res) {
-  // err - an error object, will be non-null if there was some error
-  //       connecting to the DB. ex. DB not running, config is incorrect
-  // client - used to make actual queries against DB
-  // done - function to call when we are finished
-  //        returns the connection back to the pool
   pool.connect(function(err, client, done) {
     if (err) {
       console.log("Error connecting to DB", err);
       res.sendStatus(500);
       done();
     } else {
-      // no error occurred, time to query
-      // 1. sql string - the actual SQL query we want to running
-      // 2. callback - function to run after the database gives us our result
-      //               takes an error object and the result object as it's args
       client.query("SELECT * FROM tasks;", function(err, result) {
         done();
         if (err) {
@@ -39,22 +28,12 @@ router.get("/", function(req, res) {
   });
 });
 router.post("/", function(req, res) {
-  // err - an error object, will be non-null if there was some error
-  //       connecting to the DB. ex. DB not running, config is incorrect
-  // client - used to make actual queries against DB
-  // done - function to call when we are finished
-  //        returns the connection back to the pool
   pool.connect(function(err, client, done) {
     if (err) {
       console.log("Error connecting to DB", err);
       res.sendStatus(500);
       done();
     } else {
-      // no error occurred, time to query
-      // 1. sql string - the actual SQL query we want to running
-      // 2. array of data - any data we want to pass to a parameterized statement
-      // 3. callback - function to run after the database gives us our result
-      //               takes an error object and the result object as it's args
       client.query(
         "INSERT INTO tasks (task) VALUES ($1) RETURNING *;",
         [ req.body.task ],
@@ -75,25 +54,19 @@ router.post("/", function(req, res) {
 router.put("/:id", function (req, res) {
   console.log("it worked");
   pool.connect(function(err, client, done) {
-    //make sure there are no errors
     if (err) {
       console.log('Error connecting to DB', err);
       res.sendStatus(500);
       done();
     } else {
-      //no errors, making our queryies - lives on client data
-      //books is our DB $1 is a placeholder
-      //always have the same # of placeholders and spots in the array
       client.query('UPDATE tasks SET task=$2, done=$3 WHERE id = $1 RETURNING *',
                   [req.params.id, req.body.task, req.body.done ],
                   function(err, result){
-                    //done will be called for both so we can put it before the if statement
                     done();
                     if (err) {
                       console.log('Error updating task', err);
                       res.sendStatus(500);
                     } else {
-                        //have to send a confirmtation that the data was deleted
                         res.send(result.rows);
                         console.log("result.rows", result.rows);
                     }
@@ -102,27 +75,20 @@ router.put("/:id", function (req, res) {
   });
 });
 router.delete("/:id", function (req, res) {
-  //first connect to the DB
   pool.connect(function(err, client, done) {
-    //make sure there are no errors
     if (err) {
       console.log('Error connecting to DB', err);
       res.sendStatus(500);
       done();
     } else {
-      //no errors, making our queryies - lives on client data
-      //books is our DB $1 is a placeholder
-      //always have the same # of placeholders and spots in the array
       client.query('DELETE FROM tasks WHERE id = $1',
                   [req.params.id],
                   function(err, result){
-                    //done will be called for both so we can put it before the if statement
                     done();
                     if (err) {
                       console.log('Error deleting task', err);
                       res.sendStatus(500);
                     } else {
-                        //have to send a confirmtation that the data was deleted
                         res.sendStatus(204);
                     }
                   });
